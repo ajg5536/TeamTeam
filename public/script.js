0d2a16f27f8d05e051c343a1d73bdce0584f68b3
Matthew Schwartz <matthewschwartz@client-104-39-94-108.mobility.psu.edu>
2016-03-20 17:35:45 -0400

    Added base script.js and sample json file for reference

---
+++ public/script.js
@@ -0,0 +1,18 @@
+/**
+ * File - script.js
+ * Authors - Matthew Schwartz, Andrew Gieraltowski, Alex Ulisky, John McDowell
+ * Last Updated - 20 March 2016 - 17:18:00
+ * Summary - This file contains the bulk of the Javascript functions in jQuery form
+ */
+
+/**
+ * Function - sampleQuery
+ */
+
+ $(document).ready(function() {
+ 	var ref = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
+ 	ref.orderByChild("Fruit").equalTo(25).on("child_added", function(snapshot) {
+ 		console.log(snapshot.key());
+ 	});
+
+ });
